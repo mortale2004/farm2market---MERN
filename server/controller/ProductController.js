bd9d797efe4e94794c6d24fd2d54d54f36a2b630
netizenlive10 <156320185+netizenlive10@users.noreply.github.com>
2024-01-21 22:43:00 +0530

    fixed issues in cloudinary

--- server/controller/ProductController.js
+++ server/controller/ProductController.js
@@ -2,145 +2,162 @@ const { validationResult } = require("express-validator");
 const { Product } = require("../models/productModel");
 const uploadCloud = require("../utils/cloudinary");
 const fs = require("fs");
-const {User} = require("../models/userModel");
-const {Address} = require("../models/addressModel");
+const { User } = require("../models/userModel");
+const { Address } = require("../models/addressModel");
 const { default: mongoose } = require("mongoose");
 
-
-
 const getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        return res.status(200).json({ status: "success", result: products });
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ status: "error", result: ["अंतर्गत सर्व्हर त्रुटी!"] })
-    }
-}
+  try {
+    const products = await Product.find();
+    return res.status(200).json({ status: "success", result: products });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: "error", result: ["अंतर्गत सर्व्हर त्रुटी!"] });
+  }
+};
 
 const getUserAllProducts = async (req, res) => {
-
-    try {
-        const products = await Product.find({userId: req.user.id});
-        return res.status(200).json({ status: "success", result: products });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ status: "error", result: ["अंतर्गत सर्व्हर त्रुटी!"] })
-    }
-}
+  try {
+    const products = await Product.find({ userId: req.user.id });
+    return res.status(200).json({ status: "success", result: products });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: "error", result: ["अंतर्गत सर्व्हर त्रुटी!"] });
+  }
+};
 
 const getOneProduct = async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
+  try {
+    const product = await Product.findById(req.params.id);
 
-        if (!product) {
-            return res.status(404).json({ status: "error", result: ["उत्पादन आढळले नाही!"] });
-        }
-
-        return res.status(201).json({ status: "success", result: [product] });
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ status: "error", result: ["अंतर्गत सर्व्हर त्रुटी!"] })
+    if (!product) {
+      return res
+        .status(404)
+        .json({ status: "error", result: ["उत्पादन आढळले नाही!"] });
     }
-}
 
+    return res.status(201).json({ status: "success", result: [product] });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: "error", result: ["अंतर्गत सर्व्हर त्रुटी!"] });
+  }
+};
 
 const createProduct = async (req, res) => {
+  const images = [];
+  const files = req.files;
 
-    const images = [];
-    const files = req.files;
-
-    if (!validationResult(req).isEmpty()) {
-
-        for (const file of files) {
-            const { path } = file;
-            fs.unlinkSync(path);
-        }
-
-        const result = validationResult(req).errors.map(m => m.msg[0]);
-        return res.status(400).json({ status: "error", result: result })
+  if (!validationResult(req).isEmpty()) {
+    for (const file of files) {
+      const { path } = file;
+      fs.unlinkSync(path);
     }
 
-    try {
+    const result = validationResult(req).errors.map((m) => m.msg[0]);
+    return res.status(400).json({ status: "error", result: result });
+  }
 
-        for (const file of files) {
-            const { path } = file;
-            const result = await uploadCloud(path);
-            images.push(result.url);
-            fs.unlinkSync(path);
-        }
-        
-        req.body.userId = req.user.id;
-
-        const product = await Product.create({ ...req.body, images: images , address: new mongoose.mongo.ObjectId(req.body.address)});
-
-
-        let user = await User.findById(req.user.id);
-
-        
-        user.sell.push(product._id)
-        user = await User.findByIdAndUpdate(req.user.id, user, {new: true});
-
-        return res.status(201).json({ status: "success", result: [product] });
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ status: "error", result: ["अंतर्गत सर्व्हर त्रुटी!"] })
+  try {
+    for (const file of files) {
+      const { path } = file;
+      const result = await uploadCloud(path);
+      images.push(result.url);
+      fs.unlinkSync(path);
     }
-}
+
+    req.body.userId = req.user.id;
+
+    const product = await Product.create({
+      ...req.body,
+      images: images,
+      address: new mongoose.mongo.ObjectId(req.body.address),
+    });
+
+    console.log("product created");
+    let user = await User.findById(req.user.id);
+
+    user.sell.push(product._id);
+    user = await User.findByIdAndUpdate(req.user.id, user, { new: true });
+
+    return res.status(201).json({ status: "success", result: [product] });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: "error", result: ["अंतर्गत सर्व्हर त्रुटी!"] });
+  }
+};
 
 const updateProduct = async (req, res) => {
-    if (!validationResult(req).isEmpty()) {
-        const result = validationResult(req).errors.map(m => m.msg[0]);
-        return res.status(400).json({ status: "error", result: result })
+  if (!validationResult(req).isEmpty()) {
+    const result = validationResult(req).errors.map((m) => m.msg[0]);
+    return res.status(400).json({ status: "error", result: result });
+  }
+
+  try {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ status: "error", result: ["उत्पादन आढळले नाही!"] });
     }
 
-    try {
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
 
-        let product = await Product.findById(req.params.id);
-
-        if (!product) {
-            return res.status(404).json({ status: "error", result: ["उत्पादन आढळले नाही!"] });
-        }
-
-        product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-
-        return res.status(201).json({ status: "success", result: [product] });
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ status: "error", result: ["अंतर्गत सर्व्हर त्रुटी!"] })
-    }
-}
+    return res.status(201).json({ status: "success", result: [product] });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: "error", result: ["अंतर्गत सर्व्हर त्रुटी!"] });
+  }
+};
 
 const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
 
-    try {
-
-        const product = await Product.findById(req.params.id);
-
-        if (!product) {
-            return res.status(404).json({ status: "error", result: ["उत्पादन आढळले नाही!"] });
-        }
-
-        let user = await User.findById(product.userId);
-
-        user.sell = user.sell.filter(s=> s.toString()!==req.params.id);
-
-        user = await User.findByIdAndUpdate(product.userId, user, {new: true});
-
-        const address = await Address.findByIdAndDelete(product.address);
-
-        await Product.findByIdAndDelete(req.params.id);
-
-        return res.status(200).json({ status: "success", result: ["यशस्वीरित्या हटवले..."] });
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ status: "error", result: ["अंतर्गत सर्व्हर त्रुटी!"] })
+    if (!product) {
+      return res
+        .status(404)
+        .json({ status: "error", result: ["उत्पादन आढळले नाही!"] });
     }
-}
 
-module.exports = { getAllProducts, getOneProduct, createProduct, updateProduct, deleteProduct, getUserAllProducts };
+    let user = await User.findById(product.userId);
+
+    user.sell = user.sell.filter((s) => s.toString() !== req.params.id);
+
+    user = await User.findByIdAndUpdate(product.userId, user, { new: true });
+
+    const address = await Address.findByIdAndDelete(product.address);
+
+    await Product.findByIdAndDelete(req.params.id);
+
+    return res
+      .status(200)
+      .json({ status: "success", result: ["यशस्वीरित्या हटवले..."] });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: "error", result: ["अंतर्गत सर्व्हर त्रुटी!"] });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getOneProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getUserAllProducts,
+};
--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,24 +1,20 @@
-
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: process.env.cloud_name,
-    api_key: process.env.api_key,
-    api_secret: process.env.api_secret
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
 });
 
-
 const uploadCloud = (filesToUpload) => {
-    return new Promise((resolve) => {
-        cloudinary.uploader.upload(filesToUpload, (error, result) => {
-            if (error) {
-                return res.send(error.message);
-            }
-
-            resolve({ url: result.secure_url }, { resource_type: "auto" });
-        });
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(filesToUpload, (error, result) => {
+      if (error) {
+        reject(error.message);
+      }
+      resolve({ url: result.secure_url }, { resource_type: "auto" });
     });
-}
+  });
+};
 
 module.exports = uploadCloud;
-
